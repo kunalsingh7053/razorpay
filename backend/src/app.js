971426490fec37ab5a50d0e8540824bfb00508aa
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-11-22 21:56:09 +0530

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,11 @@ const app = express();
 const paymentRoutes = require('./routes/payment.route');
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://razorpay-frontend-li8y.onrender.com",  // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/products',productRoutes);
 app.use('/api/payments',paymentRoutes);
